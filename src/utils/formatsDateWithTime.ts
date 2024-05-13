facdff42753c3478a638d8042bf63571570ef8da
dmytro-chushko <dmytro.chushko@gmail.com>
2024-05-13 23:38:55 +0300

    Add prittier and replace the favicon

--- src/utils/formatsDateWithTime.ts
+++ src/utils/formatsDateWithTime.ts
@@ -1,35 +1,44 @@
 import { DATE_TYPE } from "types/data.types";
 import { modifyMonth } from "./formatsMonth";
 
-export const formatsDateWithTime = (dateType: DATE_TYPE, inputDate?: string): string => {
-	const date = inputDate ? new Date(inputDate) : new Date();
-	const today = date.getDate() !== new Date().getDate();
-	const day = dateType === DATE_TYPE.SIDE_BAR ? modifyDateOrTime(date.getDate()) : date.getDate();
-	const month = date.getMonth() + 1;
-	const dayInterval = date.getHours() < 12 ? "AM" : "PM";
-	const time = `${modifyHours(modifyDateOrTime(date.getHours()))}:${modifyDateOrTime(
-		date.getMinutes(),
-	)} ${dayInterval}`;
+export const formatsDateWithTime = (
+  dateType: DATE_TYPE,
+  inputDate?: string,
+): string => {
+  const date = inputDate ? new Date(inputDate) : new Date();
+  const today = date.getDate() !== new Date().getDate();
+  const day =
+    dateType === DATE_TYPE.SIDE_BAR
+      ? modifyDateOrTime(date.getDate())
+      : date.getDate();
+  const month = date.getMonth() + 1;
+  const dayInterval = date.getHours() < 12 ? "AM" : "PM";
+  const time = `${modifyHours(
+    modifyDateOrTime(date.getHours()),
+  )}:${modifyDateOrTime(date.getMinutes())} ${dayInterval}`;
 
-	function modifyDateOrTime(value: number): string {
-		return value < 10 ? `0${value}` : `${value}`;
-	}
+  function modifyDateOrTime(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
+  }
 
-	function modifyHours(value: string): string {
-		return +value > 12 ? `${+value - 12}` : `${value}`;
-	}
+  function modifyHours(value: string): string {
+    return +value > 12 ? `${+value - 12}` : `${value}`;
+  }
 
-	if (dateType === DATE_TYPE.SIDE_BAR) {
-		return `${
-			today
-				? `${day}/${modifyDateOrTime(month)}/${date.getFullYear().toString().slice(2)} `
-				: `${time}`
-		}`;
-	}
+  if (dateType === DATE_TYPE.SIDE_BAR) {
+    return `${
+      today
+        ? `${day}/${modifyDateOrTime(month)}/${date
+            .getFullYear()
+            .toString()
+            .slice(2)} `
+        : `${time}`
+    }`;
+  }
 
-	if (dateType === DATE_TYPE.WORKSPACE) {
-		return `${modifyMonth(month)} ${day}, ${date.getFullYear()} at ${time}`;
-	}
+  if (dateType === DATE_TYPE.WORKSPACE) {
+    return `${modifyMonth(month)} ${day}, ${date.getFullYear()} at ${time}`;
+  }
 
-	return "Wrong type of date";
+  return "Wrong type of date";
 };
